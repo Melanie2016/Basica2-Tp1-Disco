7121a533feb03c21ebc65da06d6c5ad449b2f931
Melanie <Isabel@192.168.1.66>
2017-04-19 20:50:46 -0300

    Trabajo Practivo Nº 1

---
+++ Tp1Disco/src/ar/edu/unlam/r3/basica2/tp1/disco/Disco.java
@@ -0,0 +1,30 @@
+package ar.edu.unlam.r3.basica2.tp1.disco;
+
+public class Disco {
+
+	private Double radioInterior;
+	private Double radioExterior;
+	
+	public Disco( Double radioInterior, Double radioExterior){
+		this.radioInterior = radioInterior;
+		this.radioExterior = radioExterior;
+		
+	}
+	
+	public Double perimetroInterior(){
+		
+		return 2 * Math.PI * this.radioInterior;
+		
+	}
+	
+	public Double perimetroExterior(){
+		
+		return 2 * Math.PI * this.radioExterior;
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ Tp1Disco/test/ar/edu/unlam/r3/basica2/tp1/disco/DiscoTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.r3.basica2.tp1.disco;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiscoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
