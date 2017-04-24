a41bde2f9d2ef5d6c8608f1b70564a17412a1d6b
Flor <Flor@Flor-PC.home>
2017-04-23 23:51:39 -0300

    Se agregan los tests requeridos

--- Tp1Disco/test/ar/edu/unlam/r3/basica2/tp1/disco/DiscoTest.java
+++ Tp1Disco/test/ar/edu/unlam/r3/basica2/tp1/disco/DiscoTest.java
@@ -2,13 +2,37 @@ package ar.edu.unlam.r3.basica2.tp1.disco;
 
 import static org.junit.Assert.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class DiscoTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testQueSeCreeUnDiscoConUnDeterminadoRadioInteriorYExterior() {
+		Disco miDisco = new Disco (2.0,4.0);
+	    Assert.assertEquals(2.0,miDisco.getRadioInterior(),0.1);
+	    
+	    Assert.assertEquals(4.0,miDisco.getRadioExterior(),0.1);
 	}
+	
+	@Test
+	public void testQueVerifiqueElPerimetroInteriorDeUnDiscoConDeterminadoRadioInterior(){
+		Disco miDisco = new Disco (2.0,4.0);
+		Assert.assertEquals(12.566,miDisco.perimetroInterior(),0.1);
+	}
+	
+	@Test
+	public void testQueVerifiqueElPerimetroExteriorDeUnDiscoConDeterminadoRadioExterior(){
+		Disco miDisco = new Disco (2.0,4.0);
+		Assert.assertEquals(25.132,miDisco.perimetroExterior(),0.1);
+	}
+	
+	@Test
+	public void testQueVerifiqueLaSuperficieDeUnDiscoConDeterminadoRadio(){
+		Disco miDisco = new Disco (2.0,4.0);
+		Assert.assertEquals(50.265, miDisco.superficie(),0.1);
+	}
+	
+	
 
 }
